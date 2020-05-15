7b8cb4c9310df3a9082033ed1008d475a5cf69a4
kim0009 <weverton1995@hotmail.com>
2020-05-14 23:39:04 -0300

    consumindo swApi e buscando qtd de filmes por planetas

--- src/main/java/br/com/desafio/DesafioStarWarsApplication.java
+++ src/main/java/br/com/desafio/DesafioStarWarsApplication.java
@@ -2,9 +2,13 @@ package br.com.desafio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 import br.com.desafio.infrastructure.mapper.DozerMappingConfig;
 
+@EnableFeignClients
+@EnableAsync
 @SpringBootApplication
 public class DesafioStarWarsApplication {
 
